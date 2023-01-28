90e9ddfbdad8055b701ff976218c4afa0ab09110
Kibirige Kalule Samuel <samuelkibirigek@gmail.com>
2023-01-28 14:17:54 +0300

    Ensuring I can access the weather condition of each hour in json file

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ parameters = {
     "lat": 0.264850,
     "lon": 32.566800,
     "appid": "69f04e4613056b159c2761a9d9e664d2",
+    "exclude": "current,daily"
 }
 
 response = requests.get("https://api.openweathermap.org/data/2.5/onecall", params=parameters)
 
-print(response.json())
+day_weather = response.json()["hourly"][slice(12)]
+condition_list = []
+for hour in day_weather:
+    condition_list.append(hour["weather"][0]["id"])
+
+print(condition_list)
 
